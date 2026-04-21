7b4cf33e30ad1cba1066aed7d2530a0b2cfe8aa3
maxritterband <mjritterband@gmail.com>
2026-04-21 19:41:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,3 @@
 // Airtable (for memories wall)
 const AIRTABLE_TOKEN   = 'pats2g7i4eTQeCijr.6f348dc358571db5d95ab75cf51924524e1d27ca74381f0f0f039ba7c5e097f2';   // starts with "pat"
 const AIRTABLE_BASE_ID = 'appIzjo8pOj3EZZoi';           // starts with "app"
-
-// Formspree (for RSVP form)
-const RSVP_FORM_ID = 'xkokrzoq';         // e.g. "xyzabcde"
