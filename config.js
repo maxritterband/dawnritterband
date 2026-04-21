0be48a7f9a2770fd5cafe7c5c133b7249331f97e
maxritterband <mjritterband@gmail.com>
2026-04-21 18:24:01 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// =============================================
+//   AIRTABLE CONFIG — DO NOT COMMIT THIS FILE
+//   This file is listed in .gitignore and will
+//   never be uploaded to GitHub.
+// =============================================
+
+const AIRTABLE_TOKEN   = 'pats2g7i4eTQeCijr.6f348dc358571db5d95ab75cf51924524e1d27ca74381f0f0f039ba7c5e097f2';   // starts with "pat"
+const AIRTABLE_BASE_ID = 'appIzjo8pOj3EZZoi';           // starts with "app"
